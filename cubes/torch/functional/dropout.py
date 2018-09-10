a5505c19ad878eddc49d15feff66f706d0b36f5a
Ralph Tang <r33tang@uwaterloo.ca>
2018-09-10 19:17:01 -0400

    Add tied graded dropout

--- cubes/torch/functional/dropout.py
+++ cubes/torch/functional/dropout.py
@@ -4,27 +4,34 @@ import random
 from torch.autograd import Function
 import numpy as np
 import torch
+import torch.nn as nn
 
 import cubes
 
 
 class GradedDropoutFunction(Function):
 
-    def __init__(self, a, b):
+    def __init__(self, a, b, tied=False):
         super().__init__()
         self.a = a
         self.b = b
+        self.curr_u = None
+        self.tied = tied
         self.cube = cubes.load("graded_dropout.cu")
 
     def forward(self, x):
-        u = min(random.randint(self.a, self.b), x.size(1))
-        self.curr_u = u
+        if self.curr_u is None or not self.tied:
+            self.curr_u = min(random.randint(self.a, self.b), x.size(1))
+        u = self.curr_u
         grid = (x.size(0), math.ceil(x.size(2) / 64))
         block = (1, 64, 1)
         self.cube.graded_dropout_fwd_bwd(*cubes.wrap(x), self.a, self.b, u, *x.size(),
             grid=grid, block=block, stream=torch.cuda.current_stream().cuda_stream)
         torch.cuda.synchronize()
         return x
+
+    def reset(self):
+        self.curr_u = None
 
     def backward(self, grad):
         u = self.curr_u
@@ -35,6 +42,21 @@ class GradedDropoutFunction(Function):
             grid=grid, block=block, stream=torch.cuda.current_stream().cuda_stream)
         torch.cuda.synchronize()
         return grad
+
+
+class GradedDropoutModule(nn.Module):
+
+    def __init__(self, a, b, tied=False):
+        super().__init__()
+        self.a = a
+        self.b = b
+        self.fn = GradedDropoutFunction(a, b, tied=tied)
+
+    def reset(self):
+        self.fn.reset()
+
+    def forward(self, x):
+        return self.fn(x)
 
 
 def graded_dropout(x, a=0, b=None, training=False):
