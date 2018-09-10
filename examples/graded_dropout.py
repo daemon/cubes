fa4733a02058b924de7560f44f7e4e081e37bafa
Ralph Tang <r33tang@uwaterloo.ca>
2018-09-10 19:07:48 -0400

    Update graded dropout example

--- examples/graded_dropout.py
+++ examples/graded_dropout.py
@@ -1,14 +1,13 @@
-import cubes
+import cubes.torch.functional as C
 import torch
-import numpy as np
 
 
 def main():
-    t = torch.ones(3, 4, 5).cuda()
-    cubes.load("graded_dropout.cu").graded_dropout_fwd_bwd(*cubes.wrap(t), np.int32(1), np.int32(5), 
-        np.int32(2), np.int32(3), np.int32(4), np.int32(5), grid=(3, 5), block=(4, 1, 1))
-    torch.cuda.synchronize()
-    print(t)
+    x = torch.ones(1, 40, 5).cuda()
+    x.requires_grad = True
+    t = C.graded_dropout(x, a=10, b=50, training=True).sum()
+    t.backward()
+    print(x.grad)
 
 
 if __name__ == "__main__":
